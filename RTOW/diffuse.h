e44a4aeb15622478e36c5279cc7392fd6eec43d4
Tien Young <tien.young@outlook.com>
2025-11-17 01:33:56 -0500

    Fixed the issue where the diffuse tracer exceeded the maximum depth.

--- RTOW/diffuse.h
+++ RTOW/diffuse.h
@@ -9,14 +9,14 @@ struct DiffuseParams
     float3 pixel00_loc;
     float3 pixel_delta_u;
     float3 pixel_delta_v;
-    unsigned int samples_per_pixel;
-    unsigned int subframe_index;
+    uint32_t samples_per_pixel;
+    uint32_t subframe_index;
     OptixTraversableHandle handle;
 };
 
 struct DiffusePayload
 {
-    unsigned int seed;
-    unsigned int depth;
+    uint32_t seed;
+    uint32_t depth;
     float3 diffuse;
 };
--- RTOW/renderer_image.h
+++ RTOW/renderer_image.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <array>
+#include <vector>
 
 #include <cuda_runtime.h>
 
