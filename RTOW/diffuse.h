b9841db5851761d6b3537ab7c9aacb4cb866fea8
Tien Young <tien.young@outlook.com>
2025-11-18 01:41:51 -0500

    Replaced optixTrace with optixTraverse + optixInvoke.
    1. Support infinited bounces.

--- RTOW/diffuse.h
+++ RTOW/diffuse.h
@@ -17,6 +17,41 @@ struct DiffuseParams
 struct DiffusePayload
 {
     uint32_t seed;
-    uint32_t depth;
+    uint32_t done;
+    float3 origin;
+    float3 direction;
     float3 diffuse;
+};
+
+struct Onb
+{
+    __forceinline__ __device__ Onb(const float3& normal)
+    {
+        m_normal = normal;
+
+        if( fabs(m_normal.x) > fabs(m_normal.z) )
+        {
+        m_binormal.x = -m_normal.y;
+        m_binormal.y =  m_normal.x;
+        m_binormal.z =  0;
+        }
+        else
+        {
+        m_binormal.x =  0;
+        m_binormal.y = -m_normal.z;
+        m_binormal.z =  m_normal.y;
+        }
+
+        m_binormal = unit_vector(m_binormal);
+        m_tangent = cross( m_binormal, m_normal );
+    }
+
+    __forceinline__ __device__ void inverse_transform(float3& p) const
+    {
+        p = p.x*m_tangent + p.y*m_binormal + p.z*m_normal;
+    }
+
+    float3 m_tangent;
+    float3 m_binormal;
+    float3 m_normal;
 };
