2cfca0add26df13f29b5715a5611a355147c579f
Tien Young <tien.young@outlook.com>
2025-11-21 01:21:31 -0500

    Support cu file realtime refresh

--- RTOW/main.cpp
+++ RTOW/main.cpp
@@ -1,18 +1,11 @@
-#include <cstddef>
 #include <fstream>
-#include <iostream>
 #include <vector>
+#include <filesystem>
 
 #include <nvrtc.h>
 
 #include <application.h>
 
-#include "exception.h"
-#include "renderer_image.h"
-#include "renderer_background.h"
-#include "renderer_sphere.h"
-#include "renderer_normal.h"
-#include "renderer_antialiasing.h"
 #include "renderer_diffuse.h"
 
 int main(int argc, char* argv[]) 
@@ -26,7 +19,8 @@ int main(int argc, char* argv[])
 
     auto app = std::make_unique<Venusaur::Application>(image_width, image_height);
 
-    auto file = std::ifstream{"cuda/diffuse.cu"};
+    auto filePath = std::filesystem::path{"cuda/diffuse.cu"};
+    auto file = std::ifstream{filePath};
     auto source = std::string{std::istreambuf_iterator<char>{file}, std::istreambuf_iterator<char>{}};
     file.close();
 
@@ -50,12 +44,48 @@ int main(int argc, char* argv[])
     NVRTC_SAFE_CALL(nvrtcGetOptiXIRSize(program, &size));
     optixir.resize(size);
     NVRTC_SAFE_CALL(nvrtcGetOptiXIR(program, optixir.data()));
+    NVRTC_SAFE_CALL(nvrtcDestroyProgram(&program));
 
     app->SetRenderer(std::make_shared<RayTracingInOneWeekend::RendererDiffuse>(app->GetOutputBuffer(), optixir));
     
     while (app->IsRunning()) 
     {
-        app->Update();
+        auto optix7cache = std::filesystem::path("C:\\Users\\const\\AppData\\Local\\NVIDIA\\OptixCache\\optix7cache.db");
+        auto cacheTime = std::filesystem::last_write_time(optix7cache);
+        auto sourceTime = std::filesystem::last_write_time(filePath);
+        if(sourceTime > cacheTime)
+        {
+            auto filePath = std::filesystem::path{"cuda/diffuse.cu"};
+            auto file = std::ifstream{filePath};
+            auto source = std::string{std::istreambuf_iterator<char>{file}, std::istreambuf_iterator<char>{}};
+            file.close();
+
+            auto program = nvrtcProgram{};
+            NVRTC_SAFE_CALL(nvrtcCreateProgram(&program, source.c_str(), "diffuse.cu", 0, NULL, NULL));
+            const char* const options[] = {
+                "-std=c++20",
+                "-optix-ir",
+                "-IC:\\ProgramData\\NVIDIA Corporation\\OptiX SDK 9.0.0\\include",
+                "-Icuda",
+                "--use_fast_math",
+            };
+            NVRTC_SAFE_CALL(nvrtcCompileProgram(program, 5, options));
+            auto size = size_t{};
+            auto log = std::string{};
+            NVRTC_SAFE_CALL(nvrtcGetProgramLogSize(program, &size));
+            log.resize(size);
+            NVRTC_SAFE_CALL(nvrtcGetProgramLog(program, log.data()));
+            std::cerr << log.c_str() << std::endl;
+            auto optixir= std::vector<char>{};
+            NVRTC_SAFE_CALL(nvrtcGetOptiXIRSize(program, &size));
+            optixir.resize(size);
+            NVRTC_SAFE_CALL(nvrtcGetOptiXIR(program, optixir.data()));
+            NVRTC_SAFE_CALL(nvrtcDestroyProgram(&program));
+
+            app->SetRenderer(std::make_shared<RayTracingInOneWeekend::RendererDiffuse>(app->GetOutputBuffer(), optixir));
+        }
+
+        app->Update();  
     }
 
     return 0;
--- RTOW/renderer_diffuse.h
+++ RTOW/renderer_diffuse.h
@@ -294,6 +294,10 @@ namespace RayTracingInOneWeekend
 			
 			OPTIX_CHECK(optixModuleDestroy(module));
 
+			// char location[2048] = "";
+			// OPTIX_CHECK(optixDeviceContextGetCacheLocation(m_context, location, 2048));
+			// std::cout << "location:" << location << std::endl;
+
 			m_sbt = {
 				.raygenRecord = d_raygenRecord,
 				.missRecordBase = d_missRecordBase,
